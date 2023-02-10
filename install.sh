e1c865850ea3c70e629600af67fa8f190589dd45
gittestt1 <101639602+gittestt1@users.noreply.github.com>
2023-02-10 09:54:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt install axel xz-utils -y &&
 axel -n8 -a -o /usr/bin/ffmpeg.tar.xz https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz &&
-tar -xf /usr/bin/ffmpeg.tar.xz &&
+tar -xf /usr/bin/ffmpeg.tar.xz -C /usr/bin &&
 rm -rf /usr/bin/ffmpeg.tar.xz &&
 mv /usr/bin/ffmpeg-master-latest-linux64-gpl-shared /usr/bin/ffmpeg &&
 echo 'export PATH="/usr/bin/ffmpeg/bin:${PATH}"' >> /etc/profile &&
